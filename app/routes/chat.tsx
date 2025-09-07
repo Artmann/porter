dbdeecdb5eecf5b861a3fcafe686c4678294620e
Christoffer Artmann <artgaard@gmail.com>
2025-09-07 15:41:21 +0200

    feat: Add a proper layout to the chat.

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -1,11 +1,23 @@
 import { useChat } from '@ai-sdk/react'
-import { DefaultChatTransport } from 'ai'
-import { useEffect, useState } from 'react'
+import { DefaultChatTransport, type UIMessage } from 'ai'
+import {
+  forwardRef,
+  memo,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  type ReactElement
+} from 'react'
+import Markdown from 'react-markdown'
 import invariant from 'tiny-invariant'
 
 import { ChatService } from '~/chat/chat-service.server'
+import { Textarea } from '~/components/ui/textarea'
+import { Button } from '~/components/ui/button'
+import { cn } from '~/lib/utils'
 import type { Route } from './+types/chat'
-import { ScrollArea } from '~/components/ui/scroll-area'
+import { ArrowBigUp } from 'lucide-react'
 
 export async function loader({ params, request }: Route.LoaderArgs) {
   const chatId = params.chatId
@@ -38,14 +50,44 @@ export default function ChatRoute({ loaderData }: Route.ComponentProps) {
 
   const [input, setInput] = useState('')
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const formRef = useRef<HTMLFormElement>(null)
+  const chatMessagesRef = useRef<HTMLDivElement>(null)
 
-    if (input.trim()) {
-      sendMessage({ text: input })
-      setInput('')
-    }
-  }
+  const scrollToTheBottomOfTheMessages = useCallback(() => {
+    chatMessagesRef.current?.scrollTo({
+      top: chatMessagesRef.current.scrollHeight,
+      behavior: 'smooth'
+    })
+  }, [chatMessagesRef])
+
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+      if (e.key === 'Enter') {
+        if (e.shiftKey) {
+          return
+        }
+
+        e.preventDefault()
+
+        formRef.current?.requestSubmit()
+      }
+    },
+    [formRef]
+  )
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+
+      if (input.trim()) {
+        sendMessage({ text: input })
+        setInput('')
+
+        scrollToTheBottomOfTheMessages()
+      }
+    },
+    [input, sendMessage, scrollToTheBottomOfTheMessages]
+  )
 
   useEffect(
     function sendInitialPrompt() {
@@ -60,6 +102,10 @@ export default function ChatRoute({ loaderData }: Route.ComponentProps) {
     [loaderData?.initialPrompt, messages, sendMessage, status]
   )
 
+  useEffect(() => {
+    scrollToTheBottomOfTheMessages()
+  }, [])
+
   if (!loaderData?.chat) {
     return (
       <div className="p-8">
@@ -69,40 +115,112 @@ export default function ChatRoute({ loaderData }: Route.ComponentProps) {
   }
 
   return (
-    <div className="w-full max-w-4xl mx-auto">
-      <div className="flex flex-col w-full h-screen">
-        <div className="flex-1 min-h-0 p-4">
-          <ScrollArea>
-            {messages.map((message) => (
-              <div key={message.id}>
-                {message.role === 'user' ? 'User: ' : 'AI: '}
-                {message.parts.map((part: any, index: number) =>
-                  part.type === 'text' ? (
-                    <span key={index}>{part.text}</span>
-                  ) : null
-                )}
-              </div>
-            ))}
-          </ScrollArea>
+    <div className="w-full">
+      <div className="flex flex-col w-full h-screen overflow-hidden">
+        <div
+          className="w-full flex-1 min-h-0 overflow-y-auto"
+          ref={chatMessagesRef}
+        >
+          <div
+            className={`
+            w-full max-w-4xl
+            mx-auto p-4
+          `}
+          >
+            <ChatMessages messages={messages} />
+          </div>
         </div>
 
-        <div className="p-4">
-          <form onSubmit={handleSubmit}>
-            <input
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              disabled={status !== 'ready'}
-              placeholder="Say something..."
-            />
-            <button
-              type="submit"
-              disabled={status !== 'ready'}
-            >
-              Submit
-            </button>
+        <div
+          className={`
+            w-full max-w-4xl
+            mx-auto p-4
+          `}
+        >
+          <form
+            ref={formRef}
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col gap-2">
+              <div>
+                <Textarea
+                  className="max-h-[10rem]"
+                  disabled={status !== 'ready'}
+                  placeholder="Ask anything"
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                />
+              </div>
+
+              <div className="flex justify-between items-center">
+                <div></div>
+                <div>
+                  <Button
+                    disabled={status !== 'ready' || input.trim().length === 0}
+                    size="icon"
+                    type="submit"
+                  >
+                    <ArrowBigUp />
+                  </Button>
+                </div>
+              </div>
+            </div>
           </form>
         </div>
       </div>
+    </div>
+  )
+}
+
+const ChatMessages = memo(function ChatMessages({
+  messages
+}: {
+  messages: UIMessage[]
+}): ReactElement {
+  return (
+    <div className="flex flex-col gap-4">
+      {messages.map((message) => (
+        <div
+          key={message.id}
+          className={cn(
+            'w-auto max-w-2/3',
+            message.role === 'user' ? 'ml-auto' : 'mr-auto'
+          )}
+        >
+          {message.role === 'user' ? (
+            <UserMessage message={message} />
+          ) : (
+            <AssistantMessage message={message} />
+          )}
+        </div>
+      ))}
+    </div>
+  )
+})
+
+function UserMessage({ message }: { message: UIMessage }): ReactElement {
+  return (
+    <div className="bg-gray-50 px-4 py-2 rounded-md">
+      <Markdown>
+        {(message.parts || [])
+          .filter((part) => part.type === 'text')
+          .map((part) => part.text)
+          .join('\n')}
+      </Markdown>
+    </div>
+  )
+}
+
+function AssistantMessage({ message }: { message: UIMessage }): ReactElement {
+  return (
+    <div className="px-4 py-2 rounded-md">
+      <Markdown>
+        {(message.parts || [])
+          .filter((part) => part.type === 'text')
+          .map((part) => part.text)
+          .join('\n')}
+      </Markdown>
     </div>
   )
 }
